47587bb9459d28d4a43cc24cccc82cfd12e3cd0f
Cristian Gonzalez <cristian-saul-66@hotmail.com>
2022-09-22 16:38:41 -0500

    primera funcion

--- holamundo.py
+++ holamundo.py
@@ -1,5 +1,11 @@
+def sumar(a,b):
+    c = a + b
+    print("se sumaron los valores usando una funcion {}".format(c))
 s = 3
 k = 1
+sumar(s,k)
 print(f"el rpimer valor es: s={s} sumado con {k} es igual a ", s + k)
+
+
 
 
